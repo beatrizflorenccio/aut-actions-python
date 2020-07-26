29130e90a488da82a87c4e1b7ec0910e0128e81e
Maria <beatrizflorenccio@gmail.com>
2020-07-26 20:10:21 -0300

    erro fdp

--- aut-actions.py
+++ aut-actions.py
@@ -1,36 +1,7 @@
 import os
 import sys
-import urllib.request
-import json
-from pathvalidate import sanitize_filename #limpeza de caracteres especiais
 import shutil #mover arquivos
 
-def criaPastas(pastas, nomeProjeto=''):
-    nomeProjeto = sanitize_filename(nomeProjeto)
+def criaPastas() {
     
-    for pasta in pastas:
-        nomePasta = os.path.join(nomeProjeto, pasta)
-        os.makedirs(nomePasta, True)
-
-def criaArquivos(arquivos, nomeProjeto=''):
-    nomeProjeto = sanitize_filename(nomeProjeto)
-    for arquivo in arquivos:
-        fonte = arquivo["from"]
-        destino = arquivo["to"]
-        nomeArquivo = fonte.rsplit("/", 1)[-1]
-        caminhoCompleto = os.path.join(nomeProjeto, destino, nomeArquivo)
-
-        if not os.path.isfile(caminhoCompleto):
-            urllib.request.urlretrieve(fonte, caminhoCompleto)
-
-
-links = [
-    {"from": "http://127.0.0.1:5500/pastaBase/index.html", "to": "src"},
-    {"from": "http://127.0.0.1:5500/pastaBase/script.js", "to": "src/js"},
-    {"from": "http://127.0.0.1:5500/pastaBase/main.scss", "to": "src/scss"}
-    ]
-
-pastas = ['src/scss', 'src/js', 'src/assets']
-
-criaPastas(pastas, 'teste')
-criaArquivos(links, 'teste')
+}

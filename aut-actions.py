33a01ea099b7481a2ab6f551c8eea164fdc13846
Maria <beatrizflorenccio@gmail.com>
2020-07-26 22:06:58 -0300

    version 4

--- aut-actions.py
+++ aut-actions.py
@@ -1,7 +1,25 @@
 import os
 import sys
-import shutil #mover arquivos
+import shutil
 
-def criaPastas() {
+def criaPastas(nomeDoProjeto=''):
     
-}
+    # estrutura do projeto
+    estrutura = ['src', 'src\scss', 'src\js']
+
+    # Cria cada pasta da estrutura
+    num_pasta = 0
+    for pasta in estrutura:
+        
+        pastaProjeto = os.path.join(nomeDoProjeto, pasta)
+        os.makedirs(pastaProjeto, True)
+
+        # Arquivos criados
+        arquivos = ['index.html', 'main.scss', 'script.js']
+
+        file = open(arquivos[num_pasta], 'w')
+        num_pasta += 1
+
+               
+
+criaPastas('teste')
